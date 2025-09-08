cc5c33f6825cc977326372ee06d4192ca539b54b
Alexandria <145508828+AlexandriaMayer@users.noreply.github.com>
2025-09-08 03:00:27 +0000

    Starting up the site

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.querySelector('#inputName').addEventListener('input', inputName)
+
+function inputDate(){
+    const userName = document.querySelector('#inputName').value
+    return userName
+}
